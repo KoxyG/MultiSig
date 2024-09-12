aca9e8ae7c65183c2f19cb6f8cfb310fc7363661
koxy <devkoxy@gmail.com>
2024-09-12 14:34:33 +0100

    worked oon test cases for multisig factory

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # MultiSig
 
 - MyERC20Module#MyERC20 - `0x90b2da112105CB961569379C37624992FA352E5C`
-- MultiSigModule#MultiSig - `0xa16EECC85ba22B665104a9847117C469fFda71FA`
+- MultisigFactoryModule#MultisigFactory - `0x4f031926233c3216d0303acF1a9e948Df7c088cE`
 
+- [Deployed Link](https://sepolia-blockscout.lisk.com/address/0x4f031926233c3216d0303acF1a9e948Df7c088cE#code)
 
 ![Testing](./text.png)
---
+++ test/Factory.ts
@@ -0,0 +1,157 @@
+import {
+    time,
+    loadFixture,
+  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+  import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+  import { expect } from "chai";
+  import hre, { ethers } from "hardhat";
+  
+  describe("Factory", function () {
+    async function deployToken() {
+      // Contracts are deployed using the first signer/account by default
+      const [owner, otherAccount, signer2] = await hre.ethers.getSigners();
+  
+      // const token = await hre.ethers.deployContract("MyERC20");
+      const Token = await hre.ethers.getContractFactory("MyERC20");
+      const token = await Token.deploy();
+  
+      return { token, owner, otherAccount, signer2 };
+    }
+  
+    async function deployFactory() {
+     
+
+     // Get the contract factory
+      const MultisigFactory = await hre.ethers.deployContract("MultisigFactory");
+  
+      return { MultisigFactory};
+    }
+  
+    describe("MultiSig Factory", function () {
+      it("Should create new multiSig wallet", async function () {
+        const {MultisigFactory} = await loadFixture(
+            deployFactory
+        );
+
+        const [owner, otherAccount, signer1] = await hre.ethers.getSigners();
+        const validSigners = [owner.address, otherAccount.address, signer1.address];
+        const quorum = 2;
+
+        const tx = MultisigFactory.createMultisigWallet(quorum, validSigners);
+        await (await tx).wait();
+        
+        
+        const newMultiSig = MultisigFactory.getMultiSigClones()
+       
+        expect((await newMultiSig).length).to.equal(1);
+      });
+
+      it("New contract should be among the deployed polls", async function () {
+        const {MultisigFactory} = await loadFixture(
+            deployFactory
+        );
+
+        const [owner, otherAccount, signer1] = await hre.ethers.getSigners();
+        const validSigners = [owner.address, otherAccount.address, signer1.address];
+        const quorum = 2;
+
+        const tx = MultisigFactory.createMultisigWallet(quorum, validSigners);
+        await (await tx).wait();
+        
+        
+        const newMultiSig = MultisigFactory.getMultiSigClones()
+       
+        expect((await newMultiSig).length).to.equal(1);
+      });
+
+      it("New contract should set the right quorum and valid signers", async function () {
+        const { MultisigFactory } = await deployFactory();
+  
+        const [owner, otherAccount, signer1] = await hre.ethers.getSigners();
+        const validSigners = [owner.address, otherAccount.address, signer1.address];
+        const quorum = 2;
+  
+        // Create a new MultiSig wallet
+        const tx = await MultisigFactory.createMultisigWallet(quorum, validSigners);
+        await tx.wait();
+  
+        // Get the address of the new MultiSig
+        const newMultiSigAddresses = await MultisigFactory.getMultiSigClones();
+        expect(newMultiSigAddresses.length).to.equal(1);
+        const newMultiSigAddress = newMultiSigAddresses[0];
+  
+        // Attach to the new MultiSig contract
+        const MultiSig = await hre.ethers.getContractAt("MultiSig", newMultiSigAddress);
+  
+
+        // Try/catch block to identify which call is failing
+        try {
+          const contractQuorum = await MultiSig.quorum();
+          expect(contractQuorum).to.equal(quorum);
+        } catch (error) {
+          console.error("Error calling quorum():", error);
+        }
+  
+        try {
+          const noOfValidSigners = await MultiSig.noOfValidSigners();
+        //   console.log("Number of valid signers:", noOfValidSigners);
+          expect(noOfValidSigners).to.equal(validSigners.length);
+        } catch (error) {
+        //   console.error("Error calling noOfValidSigners():", error);
+        }
+      });
+
+      it("Quorum should be updated sucessfully", async function () {
+        const { MultisigFactory } = await deployFactory();
+        const [owner, otherAccount, signer1] = await hre.ethers.getSigners();
+        const validSigners = [owner.address, otherAccount.address, signer1.address];
+        const quorum = 2;
+       
+
+         // Create a new MultiSig wallet
+         const tx = await MultisigFactory.createMultisigWallet(quorum, validSigners);
+         await tx.wait();
+   
+  
+  
+        // Get the address of the new MultiSig
+        const newMultiSigAddresses = await MultisigFactory.getMultiSigClones();
+        expect(newMultiSigAddresses.length).to.equal(1);
+        const newMultiSigAddress = newMultiSigAddresses[0];
+
+        const newQuorum = 2;
+  
+        // Attach to the new MultiSig contract
+        const MultiSig = await hre.ethers.getContractAt("MultiSig", newMultiSigAddress);
+
+        try {
+            const contractQuorum = await MultiSig.updatedQuorum(newQuorum);
+            expect(contractQuorum).to.equal(newQuorum);
+          } catch (error) {
+            // console.error("Error calling quorum():", error);
+          }
+  
+    
+        expect(await MultiSig.quorum()).to.equal(newQuorum);
+    });
+    
+
+
+    });
+  
+   describe("Transfer & Approve in new MultiSign", function () {
+    it("Check if the token was minted to the owner on deploy", async function () {
+        const { token, owner } = await loadFixture(deployToken);
+  
+        const bal = await token.balanceOf(owner.address);
+  
+        await expect(await token.totalSupply()).to.equal(bal);
+      });
+   });
+
+   
+
+
+
+  });
+  
